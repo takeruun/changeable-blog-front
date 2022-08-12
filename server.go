fabf35bc44401d62eae8d71545f9e4b5a6fd1171
takeru.hashimoto <hstake25@gmail.com>
2022-08-12 22:51:42 +0900

    first commit

---
+++ server.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"app/config"
+	"app/graphql/generated"
+	"app/graphql/resolver"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/go-chi/chi"
+	"github.com/rs/cors"
+)
+
+const defaultPort = "3000"
+
+func main() {
+	router := chi.NewRouter()
+	router.Use(cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:3001"},
+		AllowCredentials: true,
+		AllowedMethods: []string{"GET", "POST", "PUT"},
+		Debug:            true,
+	}).Handler)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	db := config.NewDB()
+
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &resolver.Resolver{DB: db.Connection}}))
+
+	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	router.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, router))
+}
