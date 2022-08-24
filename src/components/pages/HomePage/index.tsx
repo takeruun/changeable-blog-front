9a031308a42146f91c812470fe799a6f9bccd82f
takeru.hashimoto <hstake25@gmail.com>
2022-08-24 23:18:45 +0900

    HomePage テンプレート

--- src/components/pages/HomePage/index.tsx
+++ src/components/pages/HomePage/index.tsx
@@ -1,5 +1,28 @@
-const HomePageContainer: React.FC = () => {
-  return <></>;
+import BlogItem from '@ui/BlogItem';
+import PostPageLayout from 'src/components/layouts/PostPageLayout';
+import { BlogType } from 'src/types/Blog';
+import styles from './styles.module.scss';
+
+type Props = {
+  blogItems: Array<BlogType>;
+};
+
+const HomePageContainer: React.FC<Props> = (props) => {
+  const { blogItems } = props;
+
+  return (
+    <PostPageLayout>
+      {blogItems.map((blogItem) => (
+        <BlogItem
+          id={blogItem.id}
+          title={blogItem.title}
+          key={blogItem.id}
+          tags={blogItem.tags.map((tag) => tag.name)}
+          date={'2022年'}
+        />
+      ))}
+    </PostPageLayout>
+  );
 };
 
 export default HomePageContainer;
