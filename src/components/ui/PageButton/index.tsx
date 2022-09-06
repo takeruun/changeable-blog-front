c85757ad5a5ad5388f9b7fd3d9f2fdf701913425
takeru.hashimoto <hstake25@gmail.com>
2022-09-06 14:39:46 +0900

    page の button

---
+++ src/components/ui/PageButton/index.tsx
@@ -0,0 +1,28 @@
+import commonClass, { CommonProps } from '../utilties/commonClass';
+import styles from './styles.module.scss';
+
+export type Props = {
+  children?: React.ReactNode;
+
+  // 選択されているかどうか
+  current: boolean;
+
+  onClick?: React.MouseEventHandler;
+} & CommonProps;
+
+const PageButton: React.FC<Props> = (props) => {
+  const { children, current, onClick } = props;
+
+  return (
+    <button
+      {...commonClass(props)}
+      className={current ? styles.page_button__current : styles.page_button}
+      onClick={onClick}
+      type='button'
+    >
+      {children}
+    </button>
+  );
+};
+
+export default PageButton;
