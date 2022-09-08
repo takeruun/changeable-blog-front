67361680480786b0872418212825f68b8af15bf2
takeru.hashimoto <hstake25@gmail.com>
2022-09-08 11:26:40 +0900

    Aside hooks

--- src/components/layouts/Aside/index.tsx
+++ src/components/layouts/Aside/index.tsx
@@ -1,13 +1,19 @@
 import Profile from './Profile';
 import Archive from './Archive';
 import styles from './styles.module.scss';
+import { useAside } from './hooks';
 import SearchField from '@ui/SearchField';
 
 const Aside: React.FC = () => {
+  const { query, handleChnageQuery } = useAside();
+
   return (
     <aside>
       <div className={styles.search}>
-        <SearchField value={''} />
+        <SearchField
+          value={query}
+          onChange={(e) => handleChnageQuery(e.target.value)}
+        />
       </div>
       <div className={styles.profile}>
         <Profile name={'橋本文瑠'} engName={'Takeru Hashimoto'} />
