6eb7c5e9f7b70540e9104c47ff4de326862c719a
takeru.hashimoto <hstake25@gmail.com>
2022-09-09 10:28:37 +0900

    e2e test server mock by msw

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,11 @@ import { ApolloProvider } from '@apollo/client';
 import type { AppProps } from 'next/app';
 import { client } from 'src/lib/apollo-client';
 
+if (process.env.MOCK_MODE === '1') {
+  const MockServer = () => import('test/mock/worker');
+  MockServer();
+}
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ApolloProvider client={client}>
