466954ecf7dc6f1540775ad6a705c0689f9d9e2c
takeru.hashimoto <hstake25@gmail.com>
2022-09-11 11:39:36 +0900

    HelpMessageIcon

---
+++ src/components/ui/HelpMessageIcon/HelpMessageIcon.stories.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { commonArgsType, commonParameters } from '../utilties/commonStorybook';
+import HelpMessageIcon, { Props } from './index';
+
+export default {
+  title: 'HelpMessageIcon',
+  component: HelpMessageIcon,
+  argTypes: {
+    ...commonArgsType,
+    message: { control: 'text' }
+  },
+  parameters: {
+    ...commonParameters.parameters
+  }
+} as ComponentMeta<typeof HelpMessageIcon>;
+
+const PageButtonTemplate: ComponentStory<typeof HelpMessageIcon> = (args) => {
+  return (
+    <div style={{ marginTop: '30px' }}>
+      <HelpMessageIcon {...args} />
+    </div>
+  );
+};
+
+export const Default = PageButtonTemplate.bind({});
+const defaultArgs: Props = {
+  message: 'メッセージ'
+};
+Default.args = defaultArgs;
