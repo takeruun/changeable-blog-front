19cfea4c283e4efbe2e2e79c36e1274a83856c83
takeru.hashimoto <hstake25@gmail.com>
2022-09-15 23:14:10 +0900

    hooks test

---
+++ src/components/pages/HomePage/hooks/__test__/index.test.ts
@@ -0,0 +1,26 @@
+import { act, screen, renderHook, waitFor } from '@testing-library/react';
+import { useRouter } from 'next/router';
+import { useTopPage } from '..';
+
+jest.mock('next/router', () => ({
+  useRouter: jest.fn()
+}));
+
+describe('useTopPage', () => {
+  describe('handleToBlogPage', () => {
+    test('ブログページへ遷移できる', async () => {
+      const push = jest.fn();
+      (useRouter as jest.Mock).mockImplementation(() => ({ push }));
+
+      const { result } = renderHook(() => useTopPage());
+      const blogId = 'blogId';
+
+      act(() => {
+        result.current.handleToBlogPage(blogId);
+      });
+
+      expect(push).toHaveBeenCalledTimes(1);
+      expect(push).toHaveBeenCalledWith(`/b/${blogId}`);
+    });
+  });
+});
